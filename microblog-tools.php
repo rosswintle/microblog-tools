3f98aca82685cc95cbf66c0d71fe84fd63303c71
Ross Wintle <ross@oikos.org.uk>
2017-08-15 07:14:02 +0100

    Initial commit of scaffolded plugin

---
+++ microblog-tools.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     micro.blog Tools
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Tools for micro.blog posting
+ * Author:          Ross Wintle/Oikos Digital
+ * Author URI:      https://rosswintle.uk
+ * Text Domain:     microblog-tools
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Microblog_Tools
+ */
